b8236450075b1cce4268de8755c98144dea5266d
AlejG2005 <droiiidpm@hotmail.com>
2024-09-19 20:46:02 -0600

    Eliminación de métodos duplicados

--- Mapa.java
+++ Mapa.java
@@ -17,21 +17,6 @@ public class Mapa {
         return peligros;
     }
 
-    public ArrayList<Peligro> getPeligrosCercanos(double latitud, double longitud, double rango) {
-        ArrayList<Peligro> peligrosCercanos = new ArrayList<>();
-        for (Peligro p : peligros) {
-            if (distancia(p.getLatitud(), p.getLongitud(), latitud, longitud) <= rango) {
-                peligrosCercanos.add(p);
-            }
-        }
-        return peligrosCercanos;
-    }
-
-    private double distancia(double lat1, double lon1, double lat2, double lon2) {
-        // Método para calcular la distancia entre dos puntos geográficos
-        return Math.sqrt(Math.pow(lat2 - lat1, 2) + Math.pow(lon2 - lon1, 2));
-    }
-
     public void agregarPeligro(Peligro peligro) {
     if (peligro != null) {
         System.out.println("Ingrese el tipo de Peligro: ");
